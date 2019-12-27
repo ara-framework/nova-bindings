44571771c4df3a9a8b076315f4e55c96305bf94c
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-12-26 20:20:52 -0600

    feat: add load script helper

--- packages/nova-helpers/src/index.js
+++ packages/nova-helpers/src/index.js
@@ -19,3 +19,23 @@ export const getData = (name, id) => {
     [DATA_ID]: id,
   });
 };
+
+export const loadScript = (src) => new Promise((resolve, reject) => {
+  if (document.querySelector(`script[src="${src}"]`)) {
+    resolve();
+
+    return;
+  }
+
+  const el = document.createElement('script');
+
+  el.type = 'text/javascript';
+  el.async = true;
+  el.src = src;
+
+  el.addEventListener('load', resolve);
+  el.addEventListener('error', reject);
+  el.addEventListener('abort', reject);
+
+  document.head.appendChild(el);
+});
